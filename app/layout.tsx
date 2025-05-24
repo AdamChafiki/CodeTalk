0bf538432b7903aa253ad09213a405d263aa255d
Chafiki <adamchafiki3@gmail.com>
2025-05-24 13:19:13 +0100

    chore: setup eslint, husky, and prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,21 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "CodeTalk",
+  title: 'CodeTalk',
   description:
-    "CodeTalk is a full-stack web application designed for developers to collaborate, ask technical questions, and share knowledge within topic-based groups.",
+    'CodeTalk is a full-stack web application designed for developers to collaborate, ask technical questions, and share knowledge within topic-based groups.',
 };
 
 export default function RootLayout({
